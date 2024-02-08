f65088d11d9b2c2ffc6fcba3d7fbc312cff63ecc
Moiseis Kaluã <moiseiskalua@gmail.com>
2024-02-08 08:09:05 -0300

    show the comments of a post

--- client/src/components/CommentSection.tsx
+++ client/src/components/CommentSection.tsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../hooks/useAppSelector";
 import { RootStateProps } from "../types/userTypes";
 import { Alert, Button, Textarea } from "flowbite-react";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import Comment from "./Comment";
+import { commentProps } from "../types/commentTypes";
 
 const CommentSection = ({postId} : {postId: string}) => {
-    console.log(postId)
+
     const { currentUser } = useAppSelector((state : RootStateProps) => state.user);
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState<string | null>('');
+    const [comments, setComments] = useState<commentProps[] | [] >([]);
+
     const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -22,14 +26,32 @@ const CommentSection = ({postId} : {postId: string}) => {
                 },
                 body: JSON.stringify({ content: comment, postId, userId: currentUser?._id})
             });
+            const data = await res.json();
             if (res.ok) {
                 setComment('');
                 setCommentError(null);
+                setComments([data, ...comments])
             }
         } catch (error) {
             setCommentError((error as Error).message)
         }
     };
+
+    useEffect(() => {
+        const getComments = async () => {
+        
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if (res.ok) {
+                    const data = await res.json();
+                    setComments(data);
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        getComments();
+    }, [postId])
 
     return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -71,6 +93,26 @@ const CommentSection = ({postId} : {postId: string}) => {
                     {commentError}
                 </Alert>}
             </form>
+        )}
+        {comments.length === 0 ? (
+            <p className="text-sm my-5">No comments yet!</p>
+        ) : (
+            <>
+                <div className="text-sm my-5 flex items-center gap-1">
+                    <p>Comments</p>
+                    <div className="border border-gray-400 py-1 px-2 rounded-sm">
+                        <p>{comments.length}</p>
+                    </div>
+                </div>
+                {
+                    comments.map((comment) => (
+                        <Comment 
+                            key={comment._id}
+                            comment={comment}
+                        />
+                    ))
+                }
+            </>
         )}
     </div>
   )
---
+++ client/src/types/commentTypes.ts
@@ -0,0 +1,6 @@
+export type commentProps = {
+    _id: string;
+    userId: string;
+    createdAt: string;
+    content: string;
+}
