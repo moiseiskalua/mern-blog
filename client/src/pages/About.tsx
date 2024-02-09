11c08f0e5d1218906b08300f5d3b40724aa0b970
Moiseis Kaluã <moiseiskalua@gmail.com>
2024-02-09 11:04:05 -0300

    complete about and projects pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,20 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div className="flex min-h-screen justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center m-20">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About Moiseis's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Moiseis's blog is a blog that I created to learn and train 
+              my programming skills and to know more about react and redux toolkit
+              to use in my professional daily baisis.
+              I hope you enjoy to see my Blog! 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,6 +1,12 @@
+import CallToAction from "../components/CallToAction"
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex items-center flex-col gap-6 m-20">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">Build fun and engaging pojects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction />
+    </div>
   )
 }
 
